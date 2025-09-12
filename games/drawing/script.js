8368bcadd74ef22df2b99e3c58a69aebf28c8179
BestSpark687090 <73793381+BestSpark687090@users.noreply.github.com>
2025-09-12 16:00:27 +0000

    fdsa

---
+++ games/drawing/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+  canvas.setAttribute("width", document.body.clientWidth + "px")
+  canvas.setAttribute("height", document.body.clientHeight + "px")
+})
